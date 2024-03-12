9dba44e5050d864f51a379727f891a5bd69fd547
ashokkumargithub1234 <ashokkumarpenjuvvi@gmail.com>
2024-03-12 08:28:00 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-import TeamMatches from "./components/ProductItem";
+import ProductItem from "./components/ProductItem";
 
 import "./App.css";
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route path="/product-item/:id" component={TeamMatches} />
+    <Route path="/product-item/:id" component={ProductItem} />
   </Switch>
 );
 
